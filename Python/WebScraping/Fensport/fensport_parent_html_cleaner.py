bc7dbee8c7f6cad8efe8df225b6ac0c7d41dc08b
Nigel Guven <nigelguven@gmail.com>
2022-09-20 23:54:06 +0100

    Update scrapers

--- Python/WebScraping/Fensport/fensport_parent_html_cleaner.py
+++ Python/WebScraping/Fensport/fensport_parent_html_cleaner.py
@@ -1,12 +1,16 @@
 # This program prints Hello, world!
 
 import os
+import re
 import uuid
+from xml.dom.minidom import Identified
 from bs4 import BeautifulSoup
 from requests import get
 
+
 try:
-    import Python.Extensions.Exceptions as exceptions 
+    import PythonScripts.Extensions.Exceptions as exceptions 
+    import PythonScripts.HTMLCleaning.scriptfunctions as functions
 except ModuleNotFoundError:
     current_working_directory = os.getcwd()
     print(current_working_directory)
@@ -14,48 +18,70 @@ except ModuleNotFoundError:
 def main():
     
     print('\n---------------------------------------------------------')
-    # Check that files are being outputted to the correct directory
+    #Go to Raw HTML directory
 
     try:
         current_working_directory = os.getcwd()
         print(current_working_directory)
-        raw_html_directory = r'Python\\HTML\\RawHTML\\TRCTuning'
+        raw_html_directory = r'Python\\HTML\\RawHTML\\FenSport'
+        processed_html_directory = r'Python\\HTML\\ProcessedHTML'
 
         if not os.path.exists(raw_html_directory):
             os.makedirs(raw_html_directory)
 
+        if not os.path.exists(processed_html_directory):
+            os.makedirs(processed_html_directory)
+
         os.chdir(raw_html_directory)
     except FileNotFoundError:
-        raise exceptions.FilePathNotCorrectException(raw_html_directory)
+        raise exceptions.FilePathNotCorrectException(raw_html_directory)       
+        
+    # Parse Directory HTML Files
+
+
+    
+
+    for path in os.listdir(os.getcwd()):
+        if(os.path.isfile(path)):
+            tmpFile = open(
+                path,
+                "r",
+                encoding="utf-8"
+            )
+            print(str(tmpFile))
+            soup = BeautifulSoup(tmpFile, 'html.parser')
+
+            # Full Product Description
+            product_view = soup.find_all("a", class_="product-card")
+            product_view = PythonScripts.HTMLCleaning.scriptfunctions.functions.remove_html_markup(product_view)
+
+            # Product Description Link
+            product_description_link = soup.find_all("a", class_="product-card")
+
+            print(product_view)
+            
+
+
+
         
         
-    # Global Variables
+            
 
-    user_agent_headers = {"User-Agent":"Mozilla/5.0"}
 
+
+    
 
     # Create File Name Parameters
-
+    """
     newGuid = str(uuid.uuid4())
-    file_name = "content_" + newGuid + ".html"      
+    file_name = "clean_fensport_content_" + newGuid + ".html"      
     print(str(file_name))
 
-    # Web Scrape
 
-    page_url = 'https://www.trc-tuning.com/TOYOTA-Tuning/GT86-FRS-BRZ-Tuning/GT86-FR-S-BRZ-Tuning/?view_mode=tiled&listing_sort=&listing_count=600'
-
-    response = get(page_url, headers = user_agent_headers)
     
-    if response.status_code == 200:
-        url_soup = str(BeautifulSoup(response.text, 'html.parser'))
-    else:
-        raise exceptions.HTTPFaultException(response.status_code)
-
-    if 'Access Denied' in url_soup:
-        raise exceptions.HTTPAccessDeniedException(response.status_code)
-
+    
     # Open new file and fill contents
-
+    
     f = open(
         file_name, 
         "w", 
@@ -63,6 +89,7 @@ def main():
 
     f.write(url_soup)
     f.close()
+    """
     print('\n---------------------------------------------------------')
     
 if __name__=="__main__":
