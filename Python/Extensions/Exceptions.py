d8d91b75446b83bf329a5a9b2200ca21ea86f1a6
Nigel Guven <nigelguven@gmail.com>
2022-09-20 01:40:41 +0100

    Push Working scraper and HTML

---
+++ Python/Extensions/Exceptions.py
@@ -0,0 +1,18 @@
+from urllib import response
+
+# define Python user-defined exceptions
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+class HTTPAccessDeniedException(Error):
+    """Website returned an error when retrieving html data. Error received for ${response}""" 
+    pass
+
+class HTTPFaultException(response):
+    """Website returned an error when retrieving html data. Error received for ${response}""" 
+    pass
+
+class FilePathNotCorrectException(str):
+    "Check that the file path is correct. Error received for path: ${str}."
+    pass
---
+++ Python/WebScraping/web_scraper.py
@@ -0,0 +1,64 @@
+# This program prints Hello, world!
+
+import os
+import uuid
+from bs4 import BeautifulSoup
+from requests import get
+
+from Python.Extensions.Exceptions import FilePathNotCorrectException, HTTPAccessDeniedException, HTTPFaultException
+
+def main():
+
+    # Check that files are being outputted to the correct directory
+
+    try:
+        current_working_directory = os.getcwd()
+        print(current_working_directory)
+        raw_html_directory = r'Python\\RawHTML\\FenSport'
+        os.chdir(raw_html_directory)
+    except FileNotFoundError:
+        raise FilePathNotCorrectException(raw_html_directory)
+        
+        
+    # Global Variables
+
+    user_agent_headers = {"User-Agent":"Mozilla/5.0"}
+    index = 1
+
+    while index < 35:
+
+        # Create File Name Parameters
+
+        newGuid = str(uuid.uuid4())
+        file_name = "content_" + newGuid + ".html"      
+        print(str(index) + ": " + file_name)
+
+        # Web Scrape
+
+        page_url = 'https://www.fensport.co.uk/collections/toyota-gt86-2012-to-2016?page='+ str(index)
+
+        response = get(page_url, headers = user_agent_headers)
+        
+        if response.status_code == 200:
+            url_soup = str(BeautifulSoup(response.text, 'html.parser'))
+        else:
+            raise HTTPFaultException(response.status_code)
+
+        if 'Access Denied' in url_soup:
+            raise HTTPAccessDeniedException(response.status_code)
+
+        # Open new file and fill contents
+
+        f = open(
+            file_name, 
+            "w", 
+            encoding = "utf-8")
+
+        f.write(url_soup)
+        f.close()
+
+        index += 1
+
+
+if __name__=="__main__":
+	main()
