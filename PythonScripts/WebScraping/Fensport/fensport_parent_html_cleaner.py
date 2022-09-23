a8803ce45244db6904df5fb7365f764c7a0c87f0
Nigel Guven <nigelguven@gmail.com>
2022-09-23 01:45:49 +0100

    Continue cleaning work

--- PythonScripts/WebScraping/Fensport/fensport_parent_html_cleaner.py
+++ PythonScripts/WebScraping/Fensport/fensport_parent_html_cleaner.py
@@ -1,18 +1,21 @@
 import os
+from pickle import FALSE
 import sys
 import csv
+import re
+
 from bs4 import BeautifulSoup
 sys.path.insert(0, 'PythonScripts\\Extensions')
+cleaner = re.compile('<.*?>') 
 
 try:
     import Exceptions as exceptions 
     import ScriptFunctions as functions
 except ModuleNotFoundError:
     current_working_directory = os.getcwd()
-    print(current_working_directory)
 
 def main():
-    
+    print('\n---------------------------------------------------------') 
     print('\n---------------------------------------------------------')
 
     try:
@@ -48,27 +51,34 @@ def main():
             dirty_product_price = str(soup.find_all("meta", property="og:price:amount"))                # All have prices
             dirty_product_description = str(soup.find_all("div", id="tab-1"))                           # All have description - needs to cleaned
             dirty_product_notes = str(soup.find_all("div", id="tab-3"))                                 # All have notes - needs to be cleaned
-            dirty_product_in_stock = str(soup.find_all("b", string='Stock status:'))     # Needs Customisation
+            dirty_product_in_stock = str(soup.find_all("span"))                                         # Needs Customisation
+                                                   
+            spanList = dirty_product_in_stock.split("span")   
 
-            if dirty_product_in_stock is None:
-                dirty_product_in_stock = str(soup.find_all("b", string='Stock status:'))
-            
-
+            clean_product_in_stock = ""
+            valueSet = False
+            for i in spanList:
+                if(valueSet == True):
+                    clean_product_in_stock = clean_product_in_stock + i
+                    break
+                if "Stock status" in i:
+                    clean_product_in_stock = i
+                    valueSet = True   
+                if "Not in Stock" in i:
+                    clean_product_in_stock = "Not in Stock"
+                    valueSet = True                                        
 
             clean_product_title = dirty_product_title.replace("[<meta content=\"", "").replace("&amp;", "&").replace("\" property=\"og:title\"/>]","").strip()
             clean_product_price = dirty_product_price.replace("[<meta content=\"", "").replace("\" property=\"og:price:amount\"/>]","").strip()
 
-            
             clean_product_description = dirty_product_description
             clean_product_notes = dirty_product_notes.replace("[<div id=\"tab-3\">","").replace("]","").split()
-            clean_product_in_stock = dirty_product_in_stock
 
 
             clean_product_supplier = str("FenSport")
 
-            if str(dirty_product_title).__contains__("Crankshaft Main Bearing Set"):
+            if str(dirty_product_title).__contains__("Crankshaft Main Bearing Set"):                
                 print('\n---------------------------------------------------------')
-                print(tmpFile)
                 print(str(clean_product_title))
                 print('\n---------------------------------------------------------')
                 print(str(clean_product_price))
@@ -77,17 +87,10 @@ def main():
                 print('\n---------------------------------------------------------')
                 print(str(clean_product_notes))
                 print('\n---------------------------------------------------------')
-                print(str(dirty_product_in_stock))
+                print(str(clean_product_in_stock))
                 print('\n---------------------------------------------------------')
                 print(str(clean_product_supplier))
-                print('\n---------------------------------------------------------')
-                print('\n---------------------------------------------------------')
-                print('\n---------------------------------------------------------')
-                print('\n---------------------------------------------------------')
-                print('\n---------------------------------------------------------')
-                print('\n---------------------------------------------------------')
-
-            
+                print('\n---------------------------------------------------------')     
             
             # Create File Name Parameters
             
