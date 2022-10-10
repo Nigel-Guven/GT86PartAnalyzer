2adb25a390bc6b4f5ab3b81be765d5920a856d8a
Nigel Guven <nigelguven@gmail.com>
2022-10-10 01:21:29 +0100

    update folder structure in domain

---
+++ Scion/GT86Domain/Fields/Stock/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT86Domain.Stock
+{
+    public class Stock
+    {
+        public enum StockClassificationType
+        {
+            Not_In_Stock = 0,
+            Preorder_Only = 1,
+            In_Stock = 2,
+            Discontinued = 3
+        }
+    }
+}
---
+++ Scion/GT86Domain/Fields/Supplier/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT86Domain.Supplier
+{
+    public class Supplier
+    {
+        public string Fen_Sport => "FenSport";
+        public string Drift_Shop => "DriftShop";
+        public string Group_D => "GroupD";
+    }
+}
