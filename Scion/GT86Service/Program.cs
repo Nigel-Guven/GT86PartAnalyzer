0adcc349340abf59d0cd6b0a36f30402741542a3
Nigel Guven <nigelguven@gmail.com>
2022-10-10 01:20:59 +0100

    add service console runner

---
+++ Scion/GT86Service/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT86Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Run Stuff here");
+        }
+    }
+}
