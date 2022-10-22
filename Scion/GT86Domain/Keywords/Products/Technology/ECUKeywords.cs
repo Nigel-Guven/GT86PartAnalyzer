452b9634751a7406ee4368783d79ed046f6054b2
Nigel Guven <nigelguven@gmail.com>
2022-10-22 04:01:51 +0100

    add ecu gauges keywords

---
+++ Scion/GT86Domain/Keywords/Products/Technology/ECUKeywords.cs
@@ -0,0 +1,65 @@
+﻿using GT86Domain.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT86Domain.Keywords.Products.Technology
+{
+    public class ECUKeywords
+    {
+        /*
+         *
+         * ECU
+         *
+         */
+        public Dictionary<string, List<string>> GetRemappingSoftwareKeywords(ECU ecu)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { ecu.Remapping_Software, new List<string>() { "Remap", ecu.Remapping_Software }}
+            };
+        }
+        public Dictionary<string, List<string>> GetECUDeviceKeywords(ECU ecu)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { ecu.ECU_Device, new List<string>() { "ecu", "device", ecu.ECU_Device }}
+            };
+        }
+        public Dictionary<string, List<string>> GetECULicenceKeywords(ECU ecu)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { ecu.ECU_Licence, new List<string>() { "ecu", "licence", ecu.ECU_Licence }}
+            };
+        }
+        public Dictionary<string, List<string>> GetPedalboxKeywords(ECU ecu)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { ecu.Pedalbox, new List<string>() { "pedalbox", "dte", "response", ecu.Pedalbox }}
+            };
+        }
+        public string Remapping_Software => "Remapping Software";
+        public string ECU_Device => "ECU Device";
+        public string ECU_Licence => "ECU Licence";
+        public string Pedalbox => "Pedalbox";
+
+        /*
+         *
+         * Logging Software
+         *
+         */
+        public string OBD_Tool => "OBD Device";
+
+        /*
+         *
+         * Cabin Devices 
+         *
+         */
+        public string Bluetooth_Device => "Bluetooth Device";
+        public string Head_Unit => "Head Unit";
+    }
+}
---
+++ Scion/GT86Domain/Keywords/Products/Technology/GaugesKeywords.cs
@@ -0,0 +1,60 @@
+﻿using GT86Domain.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT86Domain.Keywords.Products.Technology
+{
+    public class GaugesKeywords
+    {
+        /*
+         *
+         * Gauges
+         *
+         */
+        public Dictionary<string, List<string>> GetBoostGaugeKeywords(Gauges gauges)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { gauges.Boost_Gauge, new List<string>() { "Boost", "gauge", "sensor", gauges.Boost_Gauge }}
+            };
+        }
+        public Dictionary<string, List<string>> GetOilPressureGaugeKeywords(Gauges gauges)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { gauges.Oil_Pressure_Gauge, new List<string>() { "Oil", "pressure", "sensor", "gauge", gauges.Oil_Pressure_Gauge }}
+            };
+        }
+        public Dictionary<string, List<string>> GetOilTemperatureKeywords(Gauges gauges)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { gauges.Oil_Temperature_Gauge, new List<string>() { "Oil", "temperature", "sensor", "gauge", gauges.Oil_Temperature_Gauge }}
+            };
+        }
+        public Dictionary<string, List<string>> GetSwitchRelocationKitKeywords(Gauges gauges)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { gauges.Switch_Relocation_Kit, new List<string>() { "switch", "relocation", "kit", gauges.Switch_Relocation_Kit }}
+            };
+        }
+        public Dictionary<string, List<string>> GetGaugeKitKeywords(Gauges gauges)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { gauges.Gauge_Kit, new List<string>() { "kit", "gauge", "sensor", gauges.Gauge_Kit }}
+            };
+        }
+        public Dictionary<string, List<string>> GetIntakeAirTemperatureKeywords(Gauges gauges)
+        {
+            return new Dictionary<string, List<string>>()
+            {
+                { gauges.Intake_Temperature_Gauge, new List<string>() { "intake", "air", "temperature", "sensor", "gauge", gauges.Intake_Temperature_Gauge }}
+            };
+        }
+    }
+}
